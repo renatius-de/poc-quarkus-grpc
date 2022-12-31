0bf147d59864f4589f27f1d6bf3269b40b9851e6
René Six <info@renatius.de>
2022-12-31 19:19:04 +0100

    Initial commit for a quarkus project

---
+++ build.gradle.kts
@@ -0,0 +1,65 @@
+plugins {
+    id("idea")
+    id("eclipse")
+
+    kotlin("jvm") version "1.8.0"
+    kotlin("plugin.allopen") version "1.8.0"
+
+    id("io.quarkus")
+}
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
+
+val quarkusPlatformVersion: String by project
+val testContainersPlatformVersion: String by project
+
+group = "de.renatius.poc.quarkus"
+version = "0.1-SNAPSHOT"
+
+dependencies {
+    implementation(enforcedPlatform("io.quarkus.platform:quarkus-bom:${quarkusPlatformVersion}"))
+    implementation("io.quarkus:quarkus-grpc")
+    implementation("io.quarkus:quarkus-hibernate-orm")
+    implementation("io.quarkus:quarkus-flyway")
+    implementation("io.quarkus:quarkus-config-yaml")
+    implementation("io.quarkus:quarkus-kotlin")
+    implementation("io.quarkus:quarkus-jdbc-postgresql")
+    implementation("io.quarkus:quarkus-arc")
+
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    testImplementation("io.quarkus:quarkus-junit5")
+
+    testImplementation(enforcedPlatform("org.testcontainers:testcontainers-bom:${testContainersPlatformVersion}"))
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:testcontainers")
+    testImplementation("org.testcontainers:postgresql")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+tasks.withType<Test> {
+    systemProperty("java.util.logging.manager", "org.jboss.logmanager.LogManager")
+
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+}
+
+allOpen {
+    annotation("javax.ws.rs.Path")
+    annotation("javax.enterprise.context.ApplicationScoped")
+    annotation("io.quarkus.test.junit.QuarkusTest")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions.jvmTarget = JavaVersion.VERSION_17.toString()
+    kotlinOptions.javaParameters = true
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,15 @@
+pluginManagement {
+    val quarkusPlatformVersion: String by settings
+
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        mavenLocal()
+    }
+
+    plugins {
+        id("io.quarkus") version quarkusPlatformVersion
+    }
+}
+
+rootProject.name = "grpc"
