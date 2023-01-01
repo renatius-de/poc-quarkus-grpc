27ccc85361732b18fa35b076de25859bb077cb51
René Six <info@renatius.de>
2023-01-01 20:03:30 +0100

    Add library module

---
+++ library/src/main/kotlin/de/renatius/poc/quarkus/GreetingConfig.kt
@@ -0,0 +1,13 @@
+package de.renatius.poc.quarkus
+
+import io.smallrye.config.ConfigMapping
+import io.smallrye.config.WithName
+
+
+@ConfigMapping(prefix = "greeting")
+interface GreetingConfig {
+
+    @WithName("message")
+    fun message(): String?
+
+}
