fe941352746310931afa4ed633e70228a79657fd
René Six <3126016+renatius-de@users.noreply.github.com>
2023-02-03 20:08:30 +0100

    poc-quarkus-25 Update dependencies in rest submodule

--- rest/build.gradle.kts
+++ rest/build.gradle.kts
@@ -7,17 +7,22 @@ plugins {
 }
 
 val assertJVersion: String by project
+val commonLangVersion: String by project
 val dataFakerVersion: String by project
 val quarkusPlatformVersion: String by project
 
 dependencies {
+    implementation(project(":library"))
+
     implementation(enforcedPlatform("io.quarkus.platform:quarkus-bom:$quarkusPlatformVersion"))
-    implementation("io.quarkus:quarkus-smallrye-openapi")
-    implementation("io.quarkus:quarkus-config-yaml")
-    implementation("io.quarkus:quarkus-kotlin")
-    implementation("io.quarkus:quarkus-resteasy-reactive-kotlin-serialization")
     implementation("io.quarkus:quarkus-arc")
+    implementation("io.quarkus:quarkus-config-yaml")
+    implementation("io.quarkus:quarkus-hibernate-orm")
+    implementation("io.quarkus:quarkus-hibernate-orm-rest-data-panache")
+    implementation("io.quarkus:quarkus-kotlin")
     implementation("io.quarkus:quarkus-resteasy-reactive")
+    implementation("io.quarkus:quarkus-resteasy-reactive-jackson")
+    implementation("io.quarkus:quarkus-smallrye-openapi")
     testImplementation("io.quarkus:quarkus-junit5")
 
     testImplementation("io.rest-assured:rest-assured")
@@ -27,4 +32,6 @@ dependencies {
     testImplementation("org.assertj:assertj-core:$assertJVersion")
 
     testImplementation("net.datafaker:datafaker:$dataFakerVersion")
+
+    testImplementation("org.apache.commons:commons-lang3:$commonLangVersion")
 }
